2e5ba6f9d9f72a549152cde271c49d5932de58ab
Devfritz <devfritzcode@gmail.com>
2021-05-31 14:00:55 -0400

    finished info components

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { GoRepo, GoGist } from 'react-icons/go';
 import { FiUsers, FiUserPlus } from 'react-icons/fi';
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
+
+  const {githubUser} = useContext(GithubContext);
+  const {public_repos , followers , following , public_gists} = githubUser;
+  const items = [
+    {id:1,icon:<GoRepo className="icon" />,label:"repos" , value:public_repos , color:'pink'},
+    {id:2,icon:<FiUsers className="icon" />,label:"followers", value:followers,color:'green'},
+    {id:3,icon:<FiUserPlus className="icon" />,label:"following", value:following,color:'purple'},
+    {id:14,icon:<GoGist className="icon" />,label:"gists", value:public_gists, color:'yellow'}
+  ]
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+          {items.map( item => {
+            console.log(item)
+            return (
+              <Item
+                key={item.id}
+                {...item}
+              />
+            )
+          })}
+      </Wrapper>
+  </section>
+  ) 
 };
+
+const Item = ({icon,label,value,color}) => {
+  return <article className="item">
+     <span className={color}>{icon}</span>
+     <div>
+       <h3>{value}</h3>
+       <p>{label}</p>
+     </div>
+  </article>
+}
 
 const Wrapper = styled.section`
   display: grid;
@@ -29,6 +62,7 @@ const Wrapper = styled.section`
       display: grid;
       place-items: center;
       border-radius: 50%;
+
     }
     .icon {
       font-size: 1.5rem;
@@ -36,6 +70,8 @@ const Wrapper = styled.section`
     h3 {
       margin-bottom: 0;
       letter-spacing: 0;
+      color:'yellow';
+    
     }
     p {
       margin-bottom: 0;
